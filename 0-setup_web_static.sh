7bdc4639289005f596e67ec592ddbe5fa10ca4d6
Gdija <khadijaghadi00@gmail.com>
2023-12-09 12:55:04 -0800

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#sets up web server
+sudo apt-get -y update 
+sudo apt-get -y install nginx
+sudo service nginx start
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "Alx School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '39i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
