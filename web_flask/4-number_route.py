bb5937aa331dfad70b3a6971a0ee9b34739b83c8
Gdija <khadijaghadi00@gmail.com>
2023-12-29 06:45:08 -0800

    fix error

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,11 +34,9 @@ def python_route(text):
     return f'Python {text.replace("_", " ")}'
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number_route(n):
-    value = int(n)
-    if isinstance(value, int):
-        return f'{n} is a number'
+    return f'{n} is a number'
 
 
 if __name__ == "__main__":
