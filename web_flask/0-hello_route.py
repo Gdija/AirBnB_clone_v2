e6d286e619d32179d36dc5eec8029ec50d773ba3
Gdija <khadijaghadi00@gmail.com>
2023-12-29 03:22:12 -0800

    fix pep error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""starting with flask"""
+"""starting with flask
+Your web application must be listening on 0.0.0.0, port 5000
+/: display “Hello HBNB!”"""
 from flask import Flask
 
 app = Flask(__name__)
-"""name of the application's module"""
+
 
 @app.route("/", strict_slashes=False)
 def hello_route():
     """function to return hello on 0.0.0.0:5000"""
     return "Hello HBNB!"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
